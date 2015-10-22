03050de7bd927c08ae6c46b46737fe96e5b676e8
louet <kygenesis@gmail.com>
2015-10-22 15:38:41 +0900

    Initial Commit

---
+++ Catalog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Catalog
+//
+//  Created by 권영각 on 2015. 10. 22..
+//  Copyright © 2015년 LTCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
